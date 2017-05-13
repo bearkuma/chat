fd9e34e1d4c513f60cfbdeeb0c84419fc23a7a20
bearkuma <chatnodejs@yahoo.co.jp>
2017-05-14 01:44:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 window.onload = function(){
 	var enter = document.getElementById('enter');
-
+	
 	enter.onclick = function(){
 		var name = document.getElementById('name');
+		
 		if ( name.value === ''){
 			var guestNum = Math.floor(Math.random()*10000);
 			var guestName = 'ゲスト' + guestNum;
 			localStorage.setItem('chatter',guestName);
 		} else {
 			localStorage.setItem('chatter',name.value);
+		}		
+		if(name.value.length > 10){
+			alert('名前は10文字以下にして下さい');
+		} else {
+		window.location.href= "chat.html"
 		}
-		window.location.href= "chat.html"		
 	}
 }
