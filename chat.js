4adda3e40817a623b52d6eb72b8c04818118e1b4
bearkuma <chatnodejs@yahoo.co.jp>
2017-05-13 17:21:27 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -12,7 +12,7 @@ window.onload = function(){
 				domMemIn.id = userId[i];
 			member.prepend(domMemIn);
 		}
-		member.style.color = '#FF5';
+		member.style.color = '#5D5';
 	});
 	socketio.on('toAll', function(data){
 		addMsg(data.value);
